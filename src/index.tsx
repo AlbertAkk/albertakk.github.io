8a08949633e83484c2083926bec72ffcb1be6e3d
Albert Akopian <all188@mail.ru>
2023-12-14 18:44:55 -0500

    test app

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <>
-    asdadawe
-    <App />
-  </>
-)
+root.render(<App />)
