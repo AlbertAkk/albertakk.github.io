47aada2c920a8708ec01e24a688e3323cd8f6b1e
Albert Akopian <all188@mail.ru>
2023-12-14 17:57:42 -0500

    added base url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ProjectPage from './pages/ProjectPage'
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
