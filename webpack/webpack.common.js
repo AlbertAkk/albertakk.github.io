ab4ed1380a64d5d4c1a9b37129b80244bf26770e
Albert Akopian <all188@mail.ru>
2023-12-14 17:52:44 -0500

    changed directory

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -66,7 +66,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './public/index.html'),
+      template: path.resolve(__dirname, '..', './index.html'),
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
