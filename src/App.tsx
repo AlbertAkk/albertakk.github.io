d0b665f9cdef3f50ef3578ad25cae3dfd2a0909a
Albert Akopian <all188@mail.ru>
2023-12-16 18:18:51 -0500

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import Home from './pages/Home'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import './styles.scss'
 import Layout from './components/Layout'
 import ProjectsComp from './pages/ProjectsComp'
 import ProjectPage from './pages/ProjectPage'
 
 export const App = () => {
-  const envVar = process.env.PUBLIC_URL || '/architecture'
-  const basename = process.env.NODE_ENV === 'prod' ? envVar : '/'
-  console.log(envVar)
-  console.log(basename)
-  console.log(process.env.PUBLIC_URL)
+  // const envVar = process.env.PUBLIC_URL || '/architecture'
+  // const basename = process.env.NODE_ENV === 'prod' ? envVar : '/'
+  // console.log(envVar)
+  // console.log(basename)
+  // console.log(process.env.PUBLIC_URL)
 
   return (
-    <BrowserRouter basename={basename}>
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -21,6 +21,6 @@ export const App = () => {
           <Route path="projects/id/:id" element={<ProjectPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
